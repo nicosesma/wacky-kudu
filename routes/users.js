d427ac57842111f5fdc5b5f2c9eb3b0d19984d84
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-29 18:06:43 -0700

    views for user profile, user edit and index pages functional.

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(request, response) {
+  response.render('users');
+});
+
+router.get('/:id', function(request, response) {
+  response.render('profile');
+});
+
+router.get('/edit/:id', function(request, response) {
+  response.render('editUser');
+});
+
+router.post('/:id', function(request, response) {
+  response.redirect('/${:id}');
 });
 
 module.exports = router;
