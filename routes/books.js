ad19d4a9071251b1edc9fe39698ed8653e72cf0f
James D Stewart Jr <jamstewjr@gmail.com>
2016-08-31 12:38:54 -0700

    all books does not work, but add one books does.

--- routes/books.js
+++ routes/books.js
@@ -2,27 +2,45 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (request, response) => {
-  response.render('books');
-});
+  response.render('books')
+  .catch(
+    error => response.send({ error, message: error.message }) )
+})
 
 router.get('/add', (request, response) => {
-  response.render('addBook');
+  response.render('addBook')
+})
+
+router.post('/', (request, response) => {
+  const Book = request.app.get('models').Book
+  Book.create(request.body)
+  .then( book => {
+    response.redirect('/books/' + book.id)
+  }).catch(
+    error => response.send({
+      error, message: error.message }) )
 });
 
 router.get('/edit/:id', (request, response) => {
-  response.render('editBook');
+  response.render('editBook')
 });
 
 router.get('/:id', (request, response) => {
-  response.render('details');
-});
+  const Book = request.app.get('models').Book
+  Book.findById(request.params.id)
+  .then( book => {
+    response.render('details', {
+      book
+    })
+  }).catch(
+    error => response.send({
+      error, message: error.message }) )
 
-router.post('/', (request, response) => {
-  response.redirect('/books/${bookId}');
-});
+})
 
+// updating book details
 router.post('/:id', (request, response) => {
-  response.redirect('/books/${bookId}');
+  response.redirect('/books/' + book.id);
 });
 
 router.delete('/:id', (request, response) => {
