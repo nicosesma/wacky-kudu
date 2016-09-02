2b81a76db2f536b7c06ac52b86c592e1a853d36a
nicosesma <nicosm310@gmail.com>
2016-09-02 10:19:49 -0700

    search DONE

--- routes/search.js
+++ routes/search.js
@@ -2,19 +2,34 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', (request, response) => {
-  const Book = request.app.get('models').Book
+  const { Book, Author, Genre } = request.app.get('models')
   let search = request.query
   let searchOptions = Object.keys(search).map(v => search[v])
 
   console.log('h:', searchOptions)
-  Book.findAll({
-    where: { title: { ilike: '%' + searchOptions + '%' }}
-  })
+  Promise.all([
+    Book.findAll({
+      where: { title: { ilike: '%' + searchOptions + '%' }}
+    }),
+    Author.findAll({
+      where: { name: { ilike: '%' + searchOptions + '%' }}
+    }),
+    Genre.findAll({
+      where: { name: { ilike: '%' + searchOptions + '%' }}
+    })
+  ])
     .then( results => {
-      console.log('2:', results );
+      console.log('2:', results[0] );
       // const { rows: books } = results
       // console.log('3:', books);
-      response.render('books/search', { results })
+      const books = results[0]
+      const authors = results[1]
+      const genres = results[2]
+      response.render('books/search', {
+        books: books,
+        authors: authors,
+        genres: genres
+      })
     })
     .catch( error => response.send({ error, message: error.message }))
 })
